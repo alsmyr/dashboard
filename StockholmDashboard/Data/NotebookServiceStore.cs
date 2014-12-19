1a76c8ba038d56078bd66702d22f66b3d50c7f51
alsmyr <erik.alsmyr@accelrys.com>
2014-12-19 16:02:32 +0100

    dev

---
+++ StockholmDashboard/Data/NotebookServiceStore.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using StockholmDashboard.Models;
+
+namespace StockholmDashboard.Data
+{
+    public class NotebookServiceStore
+    {
+        private static List<NotebookService> _services { get; set; } 
+        static NotebookServiceStore()
+        {
+            _services = new List<NotebookService>();
+
+            var n1 = new NotebookService
+            {
+                Name = "Trunk SQL",
+                Description = "On-Prem with MS SQL Server (> 1M experiments)",
+                API_Url = "https://trunk-sql-12.accelrys.net/api/v1",
+                UI_Url = "https://trunk-sql-12.accelrys.net/notebook"
+            };
+
+            var n2 = new NotebookService
+            {
+                Name = "Trunk ORA",
+                Description = "On-Prem with Oracle (> 1M experiments)",
+                API_Url = "https://trunk-ora-12.accelrys.net/api/v1",
+                UI_Url = "https://trunk-ora-12.accelrys.net/notebook"
+            };
+
+            var n3 = new NotebookService
+            {
+                Name = "Trunk Cloud",
+                Description = "Cloud like environment",
+                API_Url = "http://trunk-cloud.accelrys.net/api/v1",
+                UI_Url = "http://trunk-cloud.accelrys.net/notebook"
+            };
+
+            _services.Add(n1);
+            _services.Add(n2);
+            _services.Add(n3);
+
+        }
+
+        public static List<NotebookService> GetServices()
+        {
+            return _services;
+        }
+    }
+}
---
+++ StockholmDashboard/Models/NotebookService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockholmDashboard.Models
+{
+    public class NotebookService
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UI_Url { get; set; }
+        public string API_Url { get; set; }
+        public string Version { get; set; }
+        public string Environment { get; set; }
+        public List<string> Integrations { get; set; }
+        public string PerformanceMetricsUrl { get; set; }
+        public string LogErrorsUrl { get; set; }
+    }
+}
